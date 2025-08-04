27ad80df76cb62361e6feb3678efe6ccaa4b5c95
Henry <henry@obamium.net>
2025-08-04 13:50:52 +0200

    add  HUB_URL and TOKEN to main

--- beszel_agent/run.sh
+++ beszel_agent/run.sh
@@ -1,4 +1,12 @@
 #!/usr/bin/with-contenv bashio
 
 KEY="$(bashio::config 'key')"
- ./usr/local/bin/agent -key "$KEY"
+HUB_URL="$(bashio::config 'hub_url')"
+TOKEN="$(bashio::config 'token')"
+
+export HUB_URL
+export TOKEN
+
+echo "[INFO] HUB_URL: $HUB_URL"
+
+/usr/local/bin/agent -key "$KEY"
