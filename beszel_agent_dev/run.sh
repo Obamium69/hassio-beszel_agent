1c892def410a530e0940446102a7d96aec12f321
Henry <henry@obamium.net>
2025-08-12 19:31:43 +0200

    allow user do define custom variables in dev

--- beszel_agent_dev/run.sh
+++ beszel_agent_dev/run.sh
@@ -9,4 +9,20 @@ export TOKEN
 
 echo "[INFO] HUB_URL: $HUB_URL"
 
+# Set vars dynamically
+if bashio::config.has_value 'environment_vars'; then
+    index=0
+    while bashio::config.exists "environment_vars[$index]"; do
+        NAME=$(bashio::config "environment_vars[$index].name")
+        VALUE=$(bashio::config "environment_vars[$index].value")
+        
+        if [[ -n "$NAME" && -n "$VALUE" ]]; then
+            export "$NAME"="$VALUE"
+
+        fi
+        
+        index=$((index + 1))
+    done
+fi
+
 /usr/local/bin/agent -key "$KEY"
